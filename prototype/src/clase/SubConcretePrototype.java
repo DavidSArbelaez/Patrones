9e217cb4d39baa98009be307516d7e2595476cef
DavidSArbelaez <davidsarbelaez@gmail.com>
2023-02-25 14:11:19 -0500

    Hola

--- prototype/src/clase/SubConcretePrototype.java
+++ prototype/src/clase/SubConcretePrototype.java
@@ -1,19 +1,30 @@
 package clase;
 /*
- * 
+ * Clase hija de ConcretePrototype
+ * Que agrega unos nuevos atributos y sus respectivos getters y setters
  */
 public class SubConcretePrototype extends ConcretePrototype{
     private String lastName;
     private String mobile;
     
+    /*
+     * Constructor vacio
+     */
     public SubConcretePrototype(){}
     
+    /*
+     * Constructor de la clase que usa tambien el contructor heredada
+     */
     public SubConcretePrototype(SubConcretePrototype cp){
         super(cp);
         this.lastName=cp.lastName;
         this.mobile=cp.mobile;
     }
     
+    /*
+     * Mètodo que sobreescribe el metodo clone de la clase padre para retornar un 
+     * objeto de esta clase hija
+     */
     @Override
     public IPrototype clone() {
         SubConcretePrototype cp=new SubConcretePrototype(this);
