b93f436fbb1657596bb6e8fbc048b27fbb2cb502
Sebaru18 <89612368+Sebaru18@users.noreply.github.com>
2023-03-16 16:19:32 -0500

    Update Solicitudes.java

--- sap/src/SAP/Solicitudes.java
+++ sap/src/SAP/Solicitudes.java
@@ -1,5 +1,32 @@
 package SAP;
 
 public class Solicitudes {
-
+	void solicitudAcademicaAdministrativa() {
+		System.out.println(" El estudiante solicito un tema academico o Administrativo");
+	}
+	
+	void preparatorio() {
+		System.out.println(" EL estudiante solicito acceder al preparatorio");
+	}
+	
+	void retiroAplazamientoReintegro() {
+		System.out.println(" EL estudiante solicito el Retiro / Aplazamiento de Materias /"
+				+ " Reintegro de la matricula ");
+	}
+	
+	void solicitudPinDEVsaldo() {
+		System.out.println(" EL estudiante solicito El pin de devolucion de Saldo");
+	}
+	
+	void solicitudPinDevolucionesExtemporaneas() {
+		System.out.println(" El estudiante Solicito la devolucion de manera extemporanea");
+	}
+	
+	void supletorioPregrado() {
+		System.out.println(" El estudiante solicito un supletorio en Pregrado");
+	}
+	
+	void solicitudRenovacionCreditoICETEX() {
+		System.out.println(" El estudiante solicito la renovacion del credito con ICETEX");
+	}
 }
