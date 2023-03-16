11b0669558ff16a04936153ef4b3573b74c5b56b
DavidSArbelaez <davidsarbelaez@gmail.com>
2023-03-16 17:52:49 -0500

    Hola

--- sap/src/SAP/Estudiantes.java
+++ sap/src/SAP/Estudiantes.java
@@ -1,21 +1,20 @@
 package SAP;
 
-
-import java.util.ArrayList;
-public class Estudiantes extends Persona{
+public class Estudiantes extends Persona {
 	int semestre;
 	String carrera;
-	ArrayList<Materia> Materias;
 	Composite composite = new Composite();
-	
-	
+
 	public Estudiantes(String nombre, int id, int semestre, String carrera) {
 		super(nombre, id);
 		this.semestre = semestre;
 		this.carrera = carrera;
-		this.Materias = new ArrayList<Materia>();
 	}
-	
+
+	public void addMateria(Materia materia) {
+		composite.añadirMateria(materia);
+
+	}
 
 	/**
 	 * @return the composite
@@ -24,23 +23,12 @@ public class Estudiantes extends Persona{
 		return composite;
 	}
 
-
 	/**
 	 * @param composite the composite to set
 	 */
 	protected void setComposite(Composite composite) {
 		this.composite = composite;
 	}
-
-
-	/**
-	 * @param materias the materias to set
-	 */
-	protected void setMaterias(ArrayList<Materia> materias) {
-		Materias = materias;
-	}
-
-
 
 	/**
 	 * @return the semestre
@@ -49,33 +37,17 @@ public class Estudiantes extends Persona{
 		return semestre;
 	}
 
-
-
-	/**
-	 * @return the materias
-	 */
-	public ArrayList<Materia> getMaterias() {
-		return Materias;
-	}
-
-
-
 	public String getCarrera() {
 		return carrera;
 	}
 
-
-
 	public void setCarrera(String carrera) {
 		this.carrera = carrera;
 	}
-
-	
 
 	@Override
 	public String toString() {
 		return "Estudiantes [carrera=" + carrera + "]";
 	}
 
-	
 }
